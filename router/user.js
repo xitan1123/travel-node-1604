6c3bd899ae35dba79c3d924452169694a3f6e6c0
ovaphlow <jsfreecss@163.com>
2016-12-02 08:39:06 +0800

    .

--- router/user.js
+++ router/user.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var router = express.Router()
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var mysql = require('./util/mysql.js');
 
 // var multer = require('multer');
 
@@ -15,6 +18,18 @@ router.route('/check')
     console.log('Cookies: ', req.cookies)
     console.log('Cookies: ', req.signedCookies)
     res.send({message: 'OK', counter: 1})
+  })
+
+// 修改密码
+router.route('/change-password')
+  .post(function (req, res) {
+    res.send({message: 'NOT_FINISHED'})
+  })
+
+// 修改昵称
+router.route('/change-nickname')
+  .post(function (req, res) {
+    res.send({message: 'NOT_FINISHED'})
   })
 
 module.exports = router
