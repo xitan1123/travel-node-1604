07a858a3d3e4be19c4bfbe2a4be259f51de20818
ovaphlow <jsfreecss@163.com>
2016-12-02 08:28:21 +0800

    推荐

--- router/wad.js
+++ router/wad.js
@@ -1,12 +1,33 @@
 var express = require('express')
+var mysql = require('../util/mysql')
+
 var router = express.Router()
 
 router.route('/recommand/:counter')
   .get(function (req, res) {
-    console.log('123')
-    console.log(req.params)
-    console.log(req.params.counter)
-    res.send({message: 'OK'})
+    mysql.pool.getConnection(function (error, connection) {
+      if (error) {
+        res.send({message: 'ERROR_ON_CONNECT_TO_DATABASE'})
+        return
+      }
+      let sql = 'select id, name, alias, taste, area, history, propose, intro, pic_1, pic_2 '
+          + 'from wine_and_dine '
+          + 'order by id desc '
+          + 'limit ?'
+      let param = [parseInt(req.params.counter), ]
+      connection.query({
+        sql: sql,
+        values: param
+      }, function (error, data) {
+        connection.release()
+        if (error) {
+          console.log(error)
+          res.send({message: 'ERROR', error: 'QUERY_FAILED'})
+          return
+        }
+        res.json(data)
+      })
+    })
   })
 
 module.exports = router
