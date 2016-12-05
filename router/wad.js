b18d5ec2332b94fdbe784d06d9fd64c1208706fe
ovaphlow <jsfreecss@163.com>
2016-12-05 10:28:18 +0800

    .

--- router/wad.js
+++ router/wad.js
@@ -3,11 +3,41 @@ var mysql = require('../util/mysql')
 
 var router = express.Router()
 
+router.route('/:id')
+  .get(function (req, res) {
+    mysql.pool.getConnection(function (err, connection) {
+      if (err) {
+        console.log(err)
+        res.send({message: 'ERROR_ON_CONNECT_TO_DATABASE'})
+        return
+      }
+      let sql = 'select id, name, alias, taste, area, history, propose, intro, pic_1, pic_2 '
+          + 'from wine_and_dine '
+          + 'where id = ? '
+          + 'limit 1'
+      let param = [parseInt(req.params.id)]
+      connection.query({
+        sql: sql,
+        values: param
+      }, function (err, data) {
+        connection.release()
+        if (err) {
+          console.log(err)
+          res.send({message: 'ERROR', error: 'QUERY_FAILED'})
+          return
+        }
+        res.json(data)
+      })
+    })
+  })
+
 router.route('/popular/:counter')
   .get(function (req, res) {
     mysql.pool.getConnection(function (err, connection) {
       if (err) {
+        console.log(err)
         res.send({message: 'ERROR_ON_CONNECT_TO_DATABASE'})
+        return
       }
       let sql = 'select wad.id, wad.name, wad.alias, wad.taste, wad.area, wad.propose, wad.pic_1, '
           + 'count(*) counter '
@@ -26,6 +56,7 @@ router.route('/popular/:counter')
         if (err) {
           console.log(err)
           res.send({message: 'ERROR', error: 'QUERY_FAILED'})
+          return
         }
         res.json(data)
       })
@@ -36,6 +67,7 @@ router.route('/recommand/:counter')
   .get(function (req, res) {
     mysql.pool.getConnection(function (error, connection) {
       if (error) {
+        console.log(error)
         res.send({message: 'ERROR_ON_CONNECT_TO_DATABASE'})
         return
       }
