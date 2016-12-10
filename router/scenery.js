6150e1001377a5be1d801bd0d5b91bba84c3864b
ovaphlow <jsfreecss@163.com>
2016-12-10 14:06:30 +0800

    指定id的单个景点
    添加注释

--- router/scenery.js
+++ router/scenery.js
@@ -3,6 +3,39 @@ var mysql = require('../util/mysql')
 
 var router = express.Router()
 
+// 指定id的单个景点数据
+router.route('/:id')
+  .get(function (request, response) {
+    mysql.pool.getConnection(function (error, connection) { if (error) {
+        console.error(error)
+        response.send({message: 'ERROR_ON_CONNECT_TO_DATABASE'})
+        return
+      }
+      let sql = `
+      select id, c.v as category, r.v as region, season, name, location, intro
+      from scenery as s, (
+        select k, v from kv where c = '景点类别'
+      ) as c, (
+        select k, v from kv where c = '景点地区'
+      ) as r
+      where s.id = ?
+        and c.k = s.category_id
+        and r.k = s.region_id
+      `
+      let param = [parseInt(request.params.id)]
+      connection.query({sql: sql, values: param}, function (error, data) {
+        connection.release()
+        if (error) {
+          console.error(error)
+          response.send({message: 'QUERY_FAILED'})
+          return
+        }
+        response.send(data)
+      })
+    })
+  })
+
+// 指定类别的所有景点
 router.route('/region/:id')
   .get(function (request, response) {
     mysql.pool.getConnection(function (error, connection) {
@@ -34,6 +67,7 @@ router.route('/region/:id')
     })
   })
 
+// 指定类别的所有景点
 router.route('/category/:id')
   .get(function (req, res) {
     mysql.pool.getConnection(function (error, connection) {
